67d0a6b0b048aa43c2a8b9e322d900647b8430d3
Jonas <Bennekou@live.dk>
2025-02-08 21:39:24 +0100

    The game can now draw the player triangle using the circleshape class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player
 from constants import *
 
 def main():
@@ -9,13 +10,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    timer = pygame.time.Clock()
+    dt = 0
     
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    draw_player = player.Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        draw_player.draw(screen)
         pygame.display.flip()
+        dt = timer.tick(60)
     
     
 if __name__ == "__main__":
